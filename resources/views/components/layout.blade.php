bba0ae3a0e95ae692ba07338864dc87afbb49669
Shifu-boop <bechanshiven@gmail.com>
2025-02-07 15:47:07 +0100

    Static pages implemtation
    first attempt to implement the static pages

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ $title ?? 'My PCO' }}</title> {{-- Dynamic page title --}}
+
+    @vite('resources/css/app.css')
+
+
+
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+          integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+          crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+{{-- Sidebar --}}
+<input type="checkbox" id="check">
+<div class="container-side-bar">
+    <label for="check">
+        <span class="fas fa-times" id="times"></span>
+        <span class="fas fa-bars" id="bars"></span>
+    </label>
+    <div class="head">Useful Sites</div>
+    <ol>
+        <li><a href="https://oer.hz.nl/" target="_blank">Course and Examination Regulations</a></li>
+        <li><a href="https://oer.hz.nl/6978cdea-fb31-430b-9bf9-63206aa07754" target="_blank">Implementation Regulations</a></li>
+        <li><a href="https://fs.myhz.nl/adfs/ls/" target="_blank">Learn Environment</a></li>
+        <li><a href="https://glaze-donut-5a5.notion.site/" target="_blank">Assignment Specifications</a></li>
+        <li><a href="https://github.com/HZ-HBO-ICT" target="_blank">GitHub Environment</a></li>
+    </ol>
+</div>
+
+{{-- Navbar --}}
+<nav class="topnav">
+    <a href="{{ route('home') }}" class="{{ request()->routeIs('home') ? 'is-active' : '' }}">Home</a>
+    <a href="{{ route('about') }}" class="{{ request()->routeIs('about') ? 'is-active' : '' }}">About</a>
+    <a href="{{ route('services') }}" class="{{ request()->routeIs('services') ? 'is-active' : '' }}">Services</a>
+    <a href="{{ route('contact') }}" class="{{ request()->routeIs('contact') ? 'is-active' : '' }}">Contact</a>
+</nav>
+
+{{-- Main Content --}}
+<main>
+    {{ $slot }} {{-- This is where child components will be inserted --}}
+</main>
+
+{{-- Footer --}}
+<footer>
+    <div class="footerContainer">
+        <div class="socialIcons">
+            <a href="https://www.facebook.com/bechan.shiven.9?locale=nl_NL" target="_blank"><i class="fa-brands fa-facebook"></i></a>
+            <a href="https://www.linkedin.com/in/shiven-bechan-256b53233/" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
+            <a href="https://x.com/?lang=nl" target="_blank"><i class="fa-brands fa-twitter"></i></a>
+            <a href="https://www.instagram.com" target="_blank"><i class="fa-brands fa-instagram"></i></a>
+            <a href="https://github.com/Shifu-boop" target="_blank"><i class="fa-brands fa-github"></i></a>
+        </div>
+        <div class="footernav">
+            <ul>
+                <li><a href="{{ url('/contact') }}">Contact</a></li>
+                <li><a href="{{ url('/faq') }}">FAQ</a></li>
+                <li><a href="{{ url('/profile') }}">Who Am I?</a></li>
+                <li><a href="{{ url('/about') }}">About</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="footerbottom">
+        <p>Copyright &copy; {{ date('Y') }}; Designed by SHIVEN</p>
+    </div>
+</footer>
+</body>
+
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,30 @@
+<x-layout title="Home">
+    <h1>My First Website</h1>
+    <p class="welcome">Welcome :)</p>
+    <img class="welcome-img" src="https://www.shutterstock.com/shutterstock/videos/1108429849/thumb/10.jpg?ip=x480"
+         alt="Welcome">
+
+    <section>
+        <h2>Why Does HBO-ICT Suit Me</h2>
+        <img src="https://scontent-ams4-1.xx.fbcdn.net/v/t39.30808-6/464727375_1101904294614311_5043987865075737416_n.jpg?stp=dst-jpg_s600x600&_nc_cat=110&ccb=1-7&_nc_sid=127cfc&_nc_ohc=r17CrZvuHFQQ7kNvgG7dEHT&_nc_zt=23&_nc_ht=scontent-ams4-1.xx&_nc_gid=As2ZLDzTzmzMPgtwIYc3_-W&oh=00_AYCLYEceBTPUNJyUMXTIf68FS_qaarGFRiEHAdcHvlkzEw&oe=672570B2"
+             class="thinking-man" alt="A man pondering about life">
+        <div class="text-box">
+            <p>Hello, my name is Shiven. I chose this course because I heard that software engineers earn a lot of
+                money. I hope you like my website as it is my first time doing something like this ever.</p>
+        </div>
+        <div class="text-box">
+            <p>Here are some more reasons why I think programming is a good fit for me:</p>
+        </div>
+        <ul class="why-it-fits">
+            <li>I like building computers.</li>
+            <img src="https://scontent-ams2-1.xx.fbcdn.net/v/t39.30808-6/464830548_1101904314614309_2479520823075691359_n.jpg?stp=dst-jpg_s600x600&_nc_cat=100&ccb=1-7&_nc_sid=127cfc&_nc_ohc=cG2mt8K2-QsQ7kNvgGwPq-B&_nc_zt=23&_nc_ht=scontent-ams2-1.xx&_nc_gid=As2ZLDzTzmzMPgtwIYc3_-W&oh=00_AYAOfetXujcIXUKgPiCae_XHbfpGneG6_B4aI9imIUmsdw&oe=672564B7"
+                 alt="A sick gaming PC" class="gaming-pc">
+            <li>I am fascinated by the way computers work.</li>
+            <img class="tree-displays"
+                 src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS95Z5BWcrBlwYhZgeZpOTPUqdjbxWJp4Goh8XZVtCRVfFAEigSpLsEap09w6XSiOiuhQ&usqp=CAU"
+                 alt="Tree on display">
+            <li>I also find it enjoyable.</li>
+        </ul>
+        <p class="main-paragrah2">Hope that was enough for you!</p>
+    </section>
+</x-layout>
