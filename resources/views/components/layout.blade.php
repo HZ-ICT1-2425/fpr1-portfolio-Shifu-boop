2a78a66c8779580b136b901f75e2b60fbe2afa8b
Shifu-boop <bechanshiven@gmail.com>
2025-02-18 09:12:42 +0100

    css fix

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title ?? 'My PCO' }}</title> {{-- Dynamic page title --}}
 
-    @vite(['/css/app.css'])
+    @vite('resources/css/app.css')
 
 
 
